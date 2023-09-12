744d421effa8b6b4cc6f0b25cac31071a62b3c85
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-12 11:10:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //call for the SayHi METHOD
+            SayHi();
+
+            AddNumbers();
+
+            //freeze console
+            Console.ReadLine();
+        }
+        static void SayHi()
+        {
+            Console.WriteLine("Your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);           
+        }
+        static void AddNumbers()
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
